0de574094fb45e2841b2fc66d8660e2fcd1c1eb3
Josh Lubawy <jlubawy@gmail.com>
2016-02-13 19:02:27 -0800

    Switch to using AVR stdio for UART input and output

--- src/main.c
+++ src/main.c
@@ -20,11 +20,14 @@
 #include <avr/io.h>
 
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+static FILE g_uart_stream = {0};
+
 static void
-serial_init( uint32_t baud )
+uart_init( uint32_t baud )
 {
     /* See table 20-1 for baud rate calculations */
     uint16_t ubrr = (F_CPU / (16*baud)) - 1;
@@ -35,42 +38,64 @@ serial_init( uint32_t baud )
     /* Disable 2x TX speed */
     UCSR0A &= ~_BV(U2X0);
 
-    /* Enable TX */
-    UCSR0B = _BV(TXEN0);
+    /* Enable RX and TX */
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0);
 
     /* Set 8-N-1 frame */
     UCSR0C = (3 << UCSZ00);
 }
 
 
-static void
-serial_putc( char c )
+static int
+uart_getc( FILE* stream )
+{
+    while ( (UCSR0A & _BV(RXC0)) == 0 ); /* wait for a character to be received */
+    return UDR0;
+}
+
+
+static int
+uart_putc( char c, FILE* stream )
 {
     while ( (UCSR0A & _BV(UDRE0)) == 0 ); /* wait for the TX buffer to be ready */
     UDR0 = c; /* put char into TX buffer */
+    return 0;
 }
 
 
 static void
-serial_println( const char* str )
+println( const char* str )
 {
     size_t i;
     size_t len = strlen( str );
 
     for ( i = 0; i < len; i++ )
     {
-        serial_putc( str[i] );
+        putchar( str[i] );
     }
 
-    serial_putc( '\r' );
-    serial_putc( '\n' );
+    putchar( '\r' );
+    putchar( '\n' );
 }
 
 
 int
 main( void )
 {
-    serial_init( 9600 );
-    serial_println( "Hello World" );
+    char c;
+
+    uart_init( 9600 );
+    fdev_setup_stream( &g_uart_stream, uart_putc, uart_getc, _FDEV_SETUP_RW );
+
+    stdin  = &g_uart_stream;
+    stdout = &g_uart_stream;
+    stderr = &g_uart_stream;
+
+    for (;;)
+    {
+        c = getchar();
+        putchar(c);
+    }
+
     return 0;
 }
